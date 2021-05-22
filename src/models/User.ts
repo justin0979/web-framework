aef3bc12a6c116348c030c6d3ff09779a18ee412
justin0979 <justinm0979@gmail.com>
2021-05-21 20:22:09 -0500

    rm submodule and add contents to root .git

---
+++ src/models/User.ts
@@ -0,0 +1,52 @@
+import axios, { AxiosResponse } from "axios";
+// well
+
+interface UserProps {
+  name?: string;
+  age?: number;
+  id?: number;
+}
+
+type Callback = () => void;
+
+export class User {
+  events: { [key: string]: Callback[] } = {};
+
+  constructor(private data: UserProps) {}
+
+  get(propName: string): string | number {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback): void {
+    const handlers = this.events[eventName] || [];
+    handlers.push(callback);
+    this.events[eventName] = handlers;
+  }
+
+  trigger(eventName: string): void {
+    const handlers = this.events[eventName];
+
+    if (!handlers || handlers.length === 0) {
+      return;
+    }
+
+    handlers.forEach((callback) => {
+      callback();
+    });
+  }
+
+  fetch(): void {
+    axios
+      .get(`http://localhost:3000/users/${this.get("id")}`)
+      .then((response: AxiosResponse): void => {
+        this.set(response.data);
+      });
+  }
+
+  save(): void {}
+}
