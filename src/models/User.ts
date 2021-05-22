11fb21c441c19493f0b593757f735df7fbb3034d
justin0979 <justinm0979@gmail.com>
2021-05-22 18:48:53 -0500

    add save() implementation for updating user and creating user on server

--- src/models/User.ts
+++ src/models/User.ts
@@ -47,5 +47,13 @@ export class User {
       });
   }
 
-  save(): void {}
+  save(): void {
+    const id = this.get("id");
+
+    if (id) {
+      axios.put(`http://localhost:3000/${id}`, this.data);
+    } else {
+      axios.post("http://localhost:3000/users", this.data);
+    }
+  }
 }
