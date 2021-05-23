9a6df8a839f3db8935c8601ce82d36d349cc8fb0
justin0979 <justinm0979@gmail.com>
2021-05-22 19:22:42 -0500

    create Eventing class to handle on(), trigger() and hold Callback type

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,5 @@
 import { User } from "./models/User";
 
-const user = new User({ id: 1 });
-
-user.set({ name: "Cow", age: 2 });
+const user = new User({ name: "Wolf", age: 9 });
 
 user.save();
--- src/models/User.ts
+++ src/models/User.ts
@@ -1,4 +1,5 @@
 import axios, { AxiosResponse } from "axios";
+import { Eventing } from "./Eventing";
 
 interface UserProps {
   name?: string;
@@ -6,11 +7,7 @@ interface UserProps {
   id?: number;
 }
 
-type Callback = () => void;
-
 export class User {
-  events: { [key: string]: Callback[] } = {};
-
   constructor(private data: UserProps) {}
 
   get(propName: string): string | number {
@@ -19,24 +16,6 @@ export class User {
 
   set(update: UserProps): void {
     Object.assign(this.data, update);
-  }
-
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-
-    handlers.forEach((callback) => {
-      callback();
-    });
   }
 
   fetch(): void {
