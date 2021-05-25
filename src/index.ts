9d85f0dac32eae17c768c58a145a6f6cef68fd42
justin0979 <justinm0979@gmail.com>
2021-05-25 12:50:06 -0500

    add and implement set method in User class to set data and send change event; add and implement fetch method in User to update db

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,7 @@ import { User } from "./models/User";
 const user = new User({ name: "Wolf", age: 9 });
 
 user.on("change", () => console.log("Changing"));
-user.trigger("change");
+
+console.log(user.get("name"));
+user.set({ name: "New Name" });
 console.log(user.get("name"));
