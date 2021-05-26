bff96f97d86ffdbf5d4490d18e584fc90c7eb792
justin0979 <justinm0979@gmail.com>
2021-05-25 22:37:30 -0500

    add Collection class with a get method

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
 import { User } from "./models/User";
 
+const rootDiv = document.getElementById("root");
+rootDiv.style.backgroundColor = "#333";
+rootDiv.style.height = "100vh";
+rootDiv.style.color = "#aaa";
+
 const user = User.buildUser({ id: 1 });
 
 user.on("change", () => console.log(user));
