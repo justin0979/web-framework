237b18dfe019a2fe669de82d2143eb969a66471c
justin0979 <justinm0979@gmail.com>
2021-05-26 12:17:55 -0500

    generalize Collection class

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,14 @@
 import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
 
 const rootDiv = document.getElementById("root");
 rootDiv.style.backgroundColor = "#333";
 rootDiv.style.height = "100vh";
 rootDiv.style.color = "#aaa";
 
-const collection = new Collection(
+const collection = new Collection<User, UserProps>(
   "http://localhost:3000/users",
+  (json: UserProps) => User.buildUser(json),
 );
 
 collection.on("change", () => {
