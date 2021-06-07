d8e0924f4a77353ace7b32081ea832d58c770dd5
justin0979 <justinm0979@gmail.com>
2021-06-06 21:13:14 -0500

    add documentation describing how get method accepts a type as an argument and using that argument (which is a key to T) to determine the return type

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,6 +1,8 @@
 export class Attributes<T> {
   constructor(private data: T) {}
 
+  // The keys of an object can be a type (see Lecture 166)
+  // K stands for KeyOfObject
   get = <K extends keyof T>(key: K): T[K] => {
     return this.data[key];
   };
