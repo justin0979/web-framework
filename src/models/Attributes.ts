00d9182a45c56476bc103b915bc1ba56604c7c05
justin0979 <justinm0979@gmail.com>
2021-06-06 21:18:52 -0500

    add detail to documentation for type of get method

--- src/models/Attributes.ts
+++ src/models/Attributes.ts
@@ -1,8 +1,13 @@
 export class Attributes<T> {
   constructor(private data: T) {}
 
-  // The keys of an object can be a type (see Lecture 166)
-  // K stands for KeyOfObject
+  // The keys of an object can be a type (see Lecture 166).
+  // K stands for KeyOfObject.
+  // 'K extends keyof T' sets up generic constraint, which
+  // limits the types that K can be.
+  // i.e. type of K can only be one of the keys of T
+  // e.g. T is UserProps, then K can only specifically be
+  // one of the following strings: "name", "age", "id"
   get = <K extends keyof T>(key: K): T[K] => {
     return this.data[key];
   };
